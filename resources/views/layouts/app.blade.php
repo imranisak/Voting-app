5f2e7c4cfc31bec038af5c929144208120127ab7
Imran Isak <info@imranisak.com>
2021-10-11 17:17:39 +0200

    Made the whole thing responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,9 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans bg-gray-background text-gray-900 text-sm">
-        <header class="flex items-center justify-between px-8 py-4">
+        <header class="flex flex-col md:flex-row items-center justify-between px-8 py-4">
             <a href="#"><img src="{{asset('img/logo.png')}}" alt="Logo"></a>
-            <div class="flex items-center">
+            <div class="flex items-center mt-2 md:mt-0">
                 @if (Route::has('login'))
                     <div class="px-6 py-4">
                         @auth
@@ -47,9 +47,9 @@
             </div>
         </header>
     <main>
-        <div class="container mx-auto max-w-custom flex" style="max-width: 1000px">
-            <div class="w-70 mr-5"><div
-                    class="bg-white sticky top-8 border-2 border-blue rounded-xl mt-16"
+        <div class="container mx-auto max-w-custom flex flex-col md:flex-row" style="max-width: 1000px">
+            <div class="w-70 mx-auto md:mx-0 md:mr-5"><div
+                    class="bg-white md:sticky md:top-8 border-2 border-blue rounded-xl mt-16"
                     style="
                           border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
                             border-image-slice: 1;
@@ -97,8 +97,8 @@
                         </div>
                     </form>
                 </div></div>
-            <div class="w-175">
-                <nav class="flex items-center justify-between text-xs">
+            <div class="w-full px-2 md:px-0 md:w-175">
+                <nav class="hidden md:flex items-center justify-between text-xs">
                     <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
                         <li><a href="#" class="border-b-4 pb-3 border-blue">All ideas (87)</a></li>
                         <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a></li>
