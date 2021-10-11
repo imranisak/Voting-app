de7bf6466d5fa4ae41e717c20bc5569ea31a4fba
Imran Isak <info@imranisak.com>
2021-10-11 15:37:58 +0200

    Finished the layout

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
+Route::view('/idea', 'show');
 //Route::get('/dashboard', function () {
 //    return view('dashboard');
 //})->middleware(['auth'])->name('dashboard');
