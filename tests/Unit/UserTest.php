cc8c6c81ae889451536eba724ed9420e6796a0b8
Imran Isak <info@imranisak.com>
2021-12-02 14:11:07 +0100

    Added tests

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function canCheckIfUserIsAnAdmin()
+    {
+        $user = User::factory()->make([
+            'name' => 'Andre',
+            'email' => 'info@imranisak.com',
+        ]);
+
+        $userB = User::factory()->make([
+            'name' => 'Andre',
+            'email' => 'user@user.com',
+        ]);
+
+        $this->assertTrue($user->isAdmin());
+        $this->assertFalse($userB->isAdmin());
+    }
+}
