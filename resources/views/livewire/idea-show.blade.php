16ab578a64aed34706b4e89de80d725e3ac0e854
Imran Isak <info@imranisak.com>
2021-12-16 13:29:09 +0100

    Added box for editing ideas

--- resources/views/livewire/idea-show.blade.php
+++ resources/views/livewire/idea-show.blade.php
@@ -43,9 +43,34 @@
                                 @click.away="isOpen = false"
                                 @keydown.escape.window="isOpen = false"
                             >
-                                <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
-                                <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Edit idea</a></li>
-                                <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Idea</a></li>
+                                <li>
+                                    <a
+                                        href="#"
+                                        class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
+                                    >
+                                        Mark as Spam
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#"
+                                       class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
+                                        @click="
+                                            isOpen=false
+                                            $dispatch('custom-show-edit-modal')
+                                        "
+                                    >
+                                        Edit idea
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#"
+                                       class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
+                                    >
+                                        Delete Idea
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     </div>
